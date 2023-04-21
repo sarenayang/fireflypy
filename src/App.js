2b06377f9f4509c957ce2f7155c35f8dbfe1849d
Sarena Yang <sarenay2@illinois.edu>
2023-04-21 12:32:31 -0500

    python app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
 function App() {
+
+  const [data, setData] = useState([{}])
+
+  useEffect(() => {
+    fetch('/members').then(
+      response => response.json()
+    ).then(
+      data => { 
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {(typeof data.members === 'undefined') ? (
+            <p>loading...</p>  ) : (
+            data.members.map((member, index) => (
+              <p key={index}>{member}</p> 
+            )
+
+          )
+        )
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
