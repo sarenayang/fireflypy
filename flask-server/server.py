b7c5d216c283494f97eb3f0a58acc98b8e0d0be0
Sarena Yang <sarenay2@illinois.edu>
2023-04-23 11:18:04 -0500

    alksdkjasldfjsdfafd its not wokringgggggg

--- flask-server/server.py
+++ flask-server/server.py
@@ -25,6 +25,7 @@ from flask import Flask, session, request, redirect, jsonify
 from flask_session import Session
 from dotenv import load_dotenv
 import spotipy
+from flask_cors import CORS
 
 load_dotenv()
 
@@ -32,38 +33,62 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(64)
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_FILE_DIR'] = './.flask_session/'
-Session(app)
+CORS(app)
 
 
-@app.route('/')
-def index():
+# @app.route('/')
+# def index():
 
+#     cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+#     auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-read-currently-playing playlist-modify-private',
+#                                                client_id=os.getenv("SPOTIPY_CLIENT_ID"),
+#                                                client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
+#                                                cache_handler=cache_handler,
+#                                                show_dialog=True)
+
+#     if request.args.get("code"):
+#         # Step 2. Being redirected from Spotify auth page
+#         auth_manager.get_access_token(request.args.get("code"))
+#         response = jsonify(auth_manager.get_access_token())
+#         return response
+#         # return redirect('http://localhost:3000')
+
+#     if not auth_manager.validate_token(cache_handler.get_cached_token()):
+#         # Step 1. Display sign in link when no token
+#         auth_url = auth_manager.get_authorize_url()
+#         return f'<h2><a href="{auth_url}">Sign in</a></h2>'
+
+#     # Step 3. Signed in, display data
+#     spotify = spotipy.Spotify(auth_manager=auth_manager)
+#     return f'<h2>Hi {spotify.me()["display_name"]}, ' \
+#            f'<small><a href="/sign_out">[sign out]<a/></small></h2>' \
+#            f'<a href="/playlists">my playlists</a> | ' \
+#            f'<a href="/currently_playing">currently playing</a> | ' \
+#         f'<a href="/current_user">me</a>' \
+
+#     # return redirect('http://localhost:3000')
+
+@app.route('/authorize')
+def accesstoken():
     cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
     auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-read-currently-playing playlist-modify-private',
                                                client_id=os.getenv("SPOTIPY_CLIENT_ID"),
                                                client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
                                                cache_handler=cache_handler,
                                                show_dialog=True)
-
-    if request.args.get("code"):
-        # Step 2. Being redirected from Spotify auth page
-        auth_manager.get_access_token(request.args.get("code"))
-        return redirect('/')
-
-    if not auth_manager.validate_token(cache_handler.get_cached_token()):
-        # Step 1. Display sign in link when no token
-        auth_url = auth_manager.get_authorize_url()
-        return f'<h2><a href="{auth_url}">Sign in</a></h2>'
-
-    # Step 3. Signed in, display data
-    spotify = spotipy.Spotify(auth_manager=auth_manager)
-    return f'<h2>Hi {spotify.me()["display_name"]}, ' \
-           f'<small><a href="/sign_out">[sign out]<a/></small></h2>' \
-           f'<a href="/playlists">my playlists</a> | ' \
-           f'<a href="/currently_playing">currently playing</a> | ' \
-        f'<a href="/current_user">me</a>' \
-
-
+    
+    auth_url = auth_manager.get_authorize_url()
+    return redirect(auth_url)
+@app.route('/callback')
+def callback():
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-read-currently-playing playlist-modify-private',
+                                               client_id=os.getenv("SPOTIPY_CLIENT_ID"),
+                                               client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
+                                               cache_handler=cache_handler,
+                                               show_dialog=True)
+    auth_manager.get_access_token(request.args.get("code"))
+    return redirect('http://localhost:3000')
 
 @app.route('/sign_out')
 def sign_out():
--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Login from './components/Login'
+// import useToken from './components/useToken'
 
 function App() {
 
-  const [data, setData] = useState([{}])
+  const [token, setToken] = useState([{}])
 
   useEffect(() => {
-    fetch('/currently_playing').then(
+    fetch('http://127.0.0.1:8080/callback').then(
       response => response.text(),
       console.log("hello")
     ).then(
       data => { 
         console.log('hello2u')
-        setData(data)
+        setToken(data.access_token)
         console.log(data)
       }
     )
   }, [])
+
+  console.log(token)
+  
   return (
     <div>
-      {(typeof data.name === 'undefined') ? (
-            <p>loading...</p>  ) : (
-            <p>{data.name}</p>
+      {/* {!token && token !=="" && token !== undefined? (
+         <a href='http://127.0.0.1:8080/accesstoken'>Log in to Spotify</a>
+        ) : (
+          <p>logged in</p>
         )
-      }
+      } */}
+      <a href='http://127.0.0.1:8080/authorize'>Log in to Spotify</a>
     </div>
   )
 }
