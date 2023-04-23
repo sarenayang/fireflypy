d2585f657561d17001240f42955937585c733b15
Sarena Yang <sarenay2@illinois.edu>
2023-04-23 13:35:27 -0500

    almost there idk

--- flask-server/server.py
+++ flask-server/server.py
@@ -20,13 +20,17 @@ Run app.py
         (will need to be updated in your Spotify app and SPOTIPY_REDIRECT_URI variable)
 """
 
+import base64
+import logging
 import os
-from flask import Flask, session, request, redirect, jsonify
+from urllib.parse import urlencode
+from flask import Flask, session, request, redirect, jsonify, url_for
 from flask_session import Session
 from dotenv import load_dotenv
+import requests
 import spotipy
 from flask_cors import CORS
-
+from spotify_client import SpotifyClient
 load_dotenv()
 
 app = Flask(__name__)
@@ -35,6 +39,11 @@ app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_FILE_DIR'] = './.flask_session/'
 CORS(app)
 
+CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
+CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
+REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
+
+spotify_client = SpotifyClient(os.getenv("SPOTIPY_CLIENT_ID"), os.getenv("SPOTIPY_CLIENT_SECRET"))
 
 # @app.route('/')
 # def index():
@@ -69,26 +78,46 @@ CORS(app)
 #     # return redirect('http://localhost:3000')
 
 @app.route('/authorize')
-def accesstoken():
+def authorize():
     cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
     auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-read-currently-playing playlist-modify-private',
                                                client_id=os.getenv("SPOTIPY_CLIENT_ID"),
                                                client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
                                                cache_handler=cache_handler,
-                                               show_dialog=True)
+                                               show_dialog=True,
+                                               redirect_uri=os.getenv("SPOTIPY_REDIRECT_URI"))
     
     auth_url = auth_manager.get_authorize_url()
     return redirect(auth_url)
+
 @app.route('/callback')
 def callback():
-    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
-    auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-read-currently-playing playlist-modify-private',
-                                               client_id=os.getenv("SPOTIPY_CLIENT_ID"),
-                                               client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
-                                               cache_handler=cache_handler,
-                                               show_dialog=True)
-    auth_manager.get_access_token(request.args.get("code"))
-    return redirect('http://localhost:3000')
+    token_url = 'https://accounts.spotify.com/api/token'
+    authorization = 'Basic ' + os.getenv("SPOTIPY_CLIENT_ID") + ':' + os.getenv("SPOTIPY_CLIENT_SECRET")
+    redirect_uri = os.getenv("SPOTIPY_REDIRECT_URI")
+    print(request.args['code'])
+    print(authorization)
+    encoded_oauth2_tokens = base64.b64encode('{}:{}'.format(CLIENT_ID, CLIENT_SECRET).encode())
+    headers = {"Authorization": "Basic {}".format(encoded_oauth2_tokens.decode())}
+    body = {'redirect_uri': redirect_uri, 
+            'grant_type': 'authorization_code',
+            'code': str(request.args.get('code'))}
+    post_response = requests.post(token_url,headers=headers,data=body)
+    if post_response.status_code == 200:
+        pr = post_response.json()
+        access_token = pr['access_token']
+        refresh_token = pr['refresh_token']
+        expires_in = pr['expires_in']
+        queryParams = {'access_token': access_token, 'refresh_token': refresh_token, 'expires_in': expires_in}
+        return redirect('http://localhost:3000?' + urlencode(queryParams))
+    else:
+        logging.error('getToken:' + str(post_response.status_code))
+        return jsonify({'error': 'invalid token'}), 400
+    # auth_token = request.args['code']
+    # spotify_client.get_authorization(auth_token)
+    # authorization_header = spotify_client.authorization_header
+    # session['authorization_header'] = authorization_header
+    # return redirect(url_for('loading_bp.loading'))
 
 @app.route('/sign_out')
 def sign_out():
